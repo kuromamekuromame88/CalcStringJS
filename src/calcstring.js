e98fd2855d641f66ae126fa8ce9407266393875e
kuromamekuromame88 <tei8sia@outlook.jp>
2026-04-23 15:25:50 +0900

    Update calcstring.js

--- src/calcstring.js
+++ src/calcstring.js
@@ -6,22 +6,19 @@ function isNumber(num){
 }
 
 class CalcString{
-  public:
-    function calc(let cstr){
-      if(typeof cstr != "String") console.error("calc関数が受け付ける第一引数はString型を期待しています。");
-      else{
-        let subc = 0;
-        let numbers = [];
-        while(subc<cstr.length){
-
-          //項から数値を抽出
-          if(isNumber(cstr[subc])){
-            if(isNumber(cstr[subc-1])) numbers[numbers.length]+=cstr[subc];
-            else numbers.push(cstr[subc]);
-          }
-          
-          subc++;
-        }
+  function calc(let cstr){
+    if(typeof cstr != "String") console.error("calc関数が受け付ける第一引数はString型を期待しています。");
+    else{
+      let subc = 0;
+      let numbers = [];
+      while(subc<cstr.length){
+        //項から数値を抽出
+        if(isNumber(cstr[subc])){
+          if(isNumber(cstr[subc-1])) numbers[numbers.length]+=cstr[subc];
+          else numbers.push(cstr[subc]);
+        }  
+        subc++;
       }
     }
+  }
 }
